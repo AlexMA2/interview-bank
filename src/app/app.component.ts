23c3f0d032cc056064a0b6925baa74290c94ecee
Alex <58560420+AlexMA2@users.noreply.github.com>
2025-08-14 23:02:12 -0500

    refactor: Configure i18n and routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BaseUrlInterceptor } from '@core/base-url.interceptor';
+import { BaseUrlInterceptor } from '@core/interceptors/base-url.interceptor';
+import { TranslateService } from '@ngx-translate/core';
+import { HeaderComponent } from '@shared/ui/header/header.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, HeaderComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     providers: [
@@ -18,4 +20,12 @@ import { BaseUrlInterceptor } from '@core/base-url.interceptor';
 })
 export class AppComponent {
     title = 'interview-bank';
+
+    private translate = inject(TranslateService);
+
+    constructor() {
+        this.translate.addLangs(['es']);
+        this.translate.setFallbackLang('es');
+        this.translate.use('es');
+    }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'products',
+        loadComponent: () =>
+            import(
+                './features/products/products-list/products-list.component'
+            ).then((c) => c.ProductsListComponent),
+    },
+    {
+        path: 'products/details',
+        loadComponent: () =>
+            import(
+                './features/products/products-details/products-details.component'
+            ).then((c) => c.ProductsDetailsComponent),
+    },
+    {
+        path: '**',
+        redirectTo: 'products',
+    },
+    {
+        path: '',
+        redirectTo: 'products',
+        pathMatch: 'full',
+    },
+];
