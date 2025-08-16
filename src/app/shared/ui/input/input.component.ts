fa14eff0f3b788208f74559bc43a4e954d26d9e4
Alex <58560420+AlexMA2@users.noreply.github.com>
2025-08-16 10:52:11 -0500

    feat(input): Add a simple input component

--- src/app/shared/ui/input/input.component.ts
+++ src/app/shared/ui/input/input.component.ts
@@ -1,46 +1,109 @@
-import { Component, forwardRef, input, signal } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { KeyValuePipe } from '@angular/common';
+import { Component, computed, inject, input, signal } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
 
 @Component({
     selector: 'app-input',
-    imports: [],
+    imports: [KeyValuePipe],
     templateUrl: './input.component.html',
     styleUrl: './input.component.scss',
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            useExisting: forwardRef(() => InputComponent),
-            multi: true,
-        },
-    ],
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
+    /**
+     * Label to be shown above the input
+     */
     public readonly label = input<string | undefined>();
+    /**
+     * Placeholder to be shown when the input is empty
+     */
     public readonly placeholder = input<string | undefined>();
+    /**
+     * Type of the input
+     */
     public readonly type = input<string>('text');
-
+    /**
+     * Hint to be shown below the input
+     */
+    public readonly hint = input<string | undefined>();
+    /**
+     * A record of validation errors where the key is the error code
+     * and the value is the corresponding message.
+     * Example: { 'required': 'Date is required.' }
+     */
+    public readonly errorMessages = input<Record<string, string> | undefined>(
+        undefined
+    );
+    /**
+     * The value of the input
+     */
     protected value = signal('');
+    /**
+     * Whether the input is disabled
+     */
     protected isDisabled = signal(false);
+    /**
+     * A unique identifier for the input
+     */
+    protected readonly customId = computed(() => crypto.randomUUID());
+    /**
+     * function for ControlValueAccessor.
+     */
+    protected onChange: (value: any) => void = () => {};
 
-    private onChange = (value: string) => {};
-    private onTouched = () => {};
+    /**
+     * function for ControlValueAccessor.
+     */
+    protected onTouched: () => void = () => {};
+    /**
+     * The ngControl for the input. Used to register the value accessor.
+     */
+    public ngControl = inject(NgControl, { optional: true, self: true });
+    /**
+     * The errors of the input
+     */
+    public get errors() {
+        return this.ngControl?.control?.errors;
+    }
 
+    constructor() {
+        if (this.ngControl) {
+            this.ngControl.valueAccessor = this;
+        }
+    }
+    /**
+     * Writes a new value from the form model to the view.
+     * @param value The new value from the form model.
+     */
     writeValue(value: string): void {
         this.value.set(value);
     }
-
+    /**
+     * Registers a function to call when the value changes.
+     * @param fn The function to register.
+     */
     registerOnChange(fn: (value: string) => void): void {
         this.onChange = fn;
     }
-
+    /**
+     * Registers a function to call when the control is touched.
+     * @param fn The function to register.
+     */
     registerOnTouched(fn: () => void): void {
         this.onTouched = fn;
     }
-
+    /**
+     * Sets the disabled state of the control.
+     * @param isDisabled Whether the control is disabled.
+     */
     setDisabledState(isDisabled: boolean): void {
         this.isDisabled.set(isDisabled);
     }
-
+    /**
+     * Handles the input event from the input element.
+     * @param event : any - DOM event
+     */
     onInput(event: any): void {
         const target = event.target as HTMLInputElement;
         const value = target.value;
@@ -48,6 +111,10 @@ export class InputComponent {
         this.onChange(value);
     }
 
+    /**
+     * Handles the blur event from the input element.
+     * Calls the onTouched function to notify the form control.
+     */
     onBlur(): void {
         this.onTouched();
     }
--- src/app/shared/ui/search-field/search-field.component.ts
+++ src/app/shared/ui/search-field/search-field.component.ts
@@ -1,7 +1,7 @@
 import { Component, input, OnDestroy, OnInit, output } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
-import { debounceTime, filter, map, pairwise, Subscription } from 'rxjs';
+import { debounceTime, filter, map, pairwise, startWith, Subscription } from 'rxjs';
 
 import { InputComponent } from '../input/input.component';
 
@@ -12,21 +12,47 @@ import { InputComponent } from '../input/input.component';
     styleUrl: './search-field.component.scss',
 })
 export class SearchFieldComponent implements OnInit, OnDestroy {
+    /**
+     * The default value of the search field. Its optional
+     */
     public readonly defaultValue = input<string | undefined>();
-    public readonly search = output<string | null>();
-    protected control = new FormControl('');
+    /**
+     * Search value event. Null if the search field is empty
+     */
+    public readonly searchValue = output<string | null>();
+    /**
+     * The form control for the search field
+     */
+    protected control = new FormControl<string | null>(this.defaultValue() ?? null);
+    /**
+     * The debounce time for the search
+     */
     private readonly debounceTime = 300;
+    /**
+     * The subscription method to unsubscribe and avoid memory leaks
+     */
     private sub?: Subscription;
 
     ngOnInit(): void {
+        this.controlValueChanges();
+    }
+
+    /**
+     * Subscribe to the value changes of the search field.
+     * Add a devounce time, set the first value to the default value, compare the value to check change
+     * and emit the new value
+     */
+    private controlValueChanges(): void {
         this.sub = this.control.valueChanges
             .pipe(
                 debounceTime(this.debounceTime),
+                startWith(this.defaultValue() ?? null),
                 pairwise(),
                 filter(([prev, next]) => prev !== next),
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
                 map(([_, next]) => next)
             )
-            .subscribe((val) => this.search.emit(val));
+            .subscribe((val) => this.searchValue.emit(val));
     }
 
     ngOnDestroy(): void {
